270179ac989973ad5b784bc49e3e3d953765d3c8
Shawn Berg <bergshawnm@gmail.com>
2021-05-08 21:48:25 -0400

    Detecting QR codes with beep confirmation

--- detect.py
+++ detect.py
@@ -1,16 +1,26 @@
 import cv2
+import json
 import urllib.request
 import time
+import requests
 
+octo_url = 'http://192.168.1.118'
 octo_api_token = '<YOUR_TOKEN_HERE>'
+
+octo_headers = {
+        'Content-Type': 'application/json',
+        'X-Api-Key': octo_api_token
+        }
+
 
 detector = cv2.QRCodeDetector()
 
 
 while True:
     #Get snapshot
-    urllib.request.urlretrieve('http://192.168.1.118/webcam/?action=snapshot', 'snapshot.jpeg')
-    time.sleep(1)
+    #urllib.request.urlretrieve('http://192.168.1.118/webcam/?action=snapshot', 'snapshot.jpeg')
+    urllib.request.urlretrieve(octo_url + '/webcam/?action=snapshot', 'snapshot.jpeg')
+    time.sleep(0.3)
     #cam = cv2.VideoCapture('http://192.168.1.118/webcam/?action=stream')
     #_, img = cam.read()
 
@@ -18,10 +28,19 @@ while True:
     #detect QR codes
     data, bbox, _ = detector.detectAndDecode(img)
 
+    #response = requests.post(octo_url+'/api/printer/command', headers=octo_headers, data=json.dumps({'command': 'M300 P500'}))
+    #print(response)
+    #print(response.text)
+
+
+
     #if QR code:
     if data:
         #Change filament to QR code if exists
         print("QR Code detected-->", data)
+        response = requests.post(octo_url+'/api/printer/command', headers=octo_headers, data=json.dumps({'command': 'M300 P500'}))
+
+        time.sleep(10)
 
         #Confirmation beep
 
@@ -32,7 +51,7 @@ while True:
     #if cv2.waitKey(1) == ord("Q"):
     #    break
 
-    time.sleep(1)
+    time.sleep(0.5)
 
 #cam.release()
 cv2.destroyAllWindows()
